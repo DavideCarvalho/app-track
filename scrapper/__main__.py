ec58c4a484726610d7e1ab2bbb96c53929bc206e
Davi de Carvalho <davicarvalho96@yahoo.com.br>
2017-09-08 16:07:52 -0300

    arrumando código

--- scrapper/__main__.py
+++ scrapper/__main__.py
@@ -9,10 +9,9 @@ from selenium.webdriver.common.keys import Keys
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    isFuture = True
-
     driver = webdriver.PhantomJS(service_args=['--ssl-protocol=any'], executable_path=r'C:\phantomjs\bin\phantomjs.exe')
     driver.get('https://www.msc.com/track-a-shipment?agencyPath=bra')
     element = driver.find_element_by_name("ctl00$ctl00$plcMain$plcMain$TrackSearch$txtBolSearch$TextField")
@@ -21,11 +20,17 @@ def index():
     text_file = open("page.txt", "w")
     text_file.write(driver.page_source)
     text_file.close()
-    tableMovements = WebDriverWait(driver, 10).until(
+    table_movements = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, "resultTable"))
     )
+    movements = get_container_movements(table_movements)
+    return Response(json.dumps(movements), mimetype='application/json')
+
+
+def get_container_movements(table_movements):
+    isFuture = True
     movements = []
-    for row in tableMovements.find_elements_by_xpath(".//tbody"):
+    for row in table_movements.find_elements_by_xpath(".//tbody"):
         print(row.text)
         for th in row.find_elements_by_xpath(".//tr"):
             movement = {}
@@ -33,7 +38,7 @@ def index():
             if time == "past":
                 if isFuture:
                     movement['time'] = "present"
-                    print("-----------------present-------------------------")
+                    print("------------------present-------------------------")
                     isFuture = False
                 else:
                     movement['time'] = th.get_attribute("class")
@@ -42,14 +47,14 @@ def index():
                 movement['time'] = th.get_attribute("class")
                 print("-----------------" + th.get_attribute("class") + "-------------------------")
             for td in th.find_elements_by_xpath(".//td"):
-                # print(td.text)
                 if td.text == " ":
                     movement[td.get_attribute('data-title')] = None
                 else:
                     movement[td.get_attribute('data-title')] = td.text
                 print(td.get_attribute('data-title') + ": " + td.text)
             movements.append(movement)
-    return Response(json.dumps(movements),  mimetype='application/json')
+    return movements
+
 
 if __name__ == '__main__':
     app.run(debug=True)
